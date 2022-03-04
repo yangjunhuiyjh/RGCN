0179a20e30a47b52f81b27e93dca74644825169c
Dylan Hillier <dylanslavinhillier@gmail.com>
2022-03-04 17:47:49 +0000

    adds the block diagonal formulation

--- Models/rgcn.py
+++ Models/rgcn.py
@@ -1,5 +1,7 @@
+import enum
+from modulefinder import Module
 from numpy import zeros
-from torch import Tensor, relu
+from torch import Tensor, relu, cat
 from torch.nn import ModuleList, Linear
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import degree
@@ -15,20 +17,29 @@ class RGCNLayer(MessagePassing):
 
             thus if num_blocks, num_bases set to None ->
                 h_i = \sum_{i,r}()
+            Note that num_blocks must factor into both in_channels and out_channels
         '''
         super().__init__(aggr='add')
-        if num_blocks is not None:
-            weight = ...
+        self.num_blocks = num_blocks
+        self.num_bases = num_bases
+        if num_blocks is not None:       
+            self.weights = ModuleList([ModuleList([Linear(in_channels//num_blocks,out_channels//num_blocks,False) for _ in range(num_blocks)]) for _ in range(num_relations)])
         elif num_bases is not None:
-            weight = ...
+            self.weights = ...
         else:
-            self.weights = ModuleList([Linear(in_channels,out_channels,bias=False)] for i in range(num_relations))
-        self.self_connection = Linear(in_channels,out_channels,False)
+            self.weights = ModuleList([Linear(in_channels,out_channels,bias=False) for i in range(num_relations)])
+            self.self_connection = Linear(in_channels,out_channels,False)
 
     def message(self,x_j,weight_r,norm):
-        return norm.view(-1,1) * weight_r(x_j)
+        if self.num_blocks is not None:
+            norm.view(-1,1) * cat([e(x_j[i*self.num_blocks:(i+1)*self.num_blocks]) for i,e in enumerate(weight_r)])
+        elif self.num_bases is not None:
+            pass
+        else:    
+            return norm.view(-1,1) * weight_r(x_j)
 
     def forward(self,x: Tensor,edge_index):
+
         out = zeros((x.size(0),self.out_channels))
         for r,e in enumerate(self.weights):
             row, col = edge_index[r]
