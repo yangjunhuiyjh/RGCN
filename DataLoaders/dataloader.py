f61c238979eb78439dc731ffacc8e5332ae8c25b
Dylan Hillier <dylanslavinhillier@gmail.com>
2022-03-23 12:46:38 +0000

    slightly changes a dataloader that wasn't working

--- DataLoaders/dataloader.py
+++ DataLoaders/dataloader.py
@@ -12,14 +12,16 @@ def get_dataset(name:str):
     elif name =="mutag":
         return Entities("Datasets/mutag","MUTAG")
     elif name == 'fb15k':
-        ds = FB15kDataset(raw_dir="Datasets/fb15k")
-        items = [from_networkx(to_networkx(item, node_attrs= item.ndata.keys(), edge_attrs=item.edata.keys())) for item in ds]
-        # ds_networkx = to_networkx(ds, node_attrs= ds.ndata.keys(), edge_attrs=ds.edata.keys())
-        # return from_networkx(ds_networkx)
-        return items
+        ds = FB15kDataset(raw_dir="Datasets/fb15k")[0]
+        # items = [from_networkx(to_networkx(item, node_attrs= item.ndata.keys(), edge_attrs=item.edata.keys())) for item in ds]
+        ds_networkx = to_networkx(ds, node_attrs= ds.ndata.keys(), edge_attrs=ds.edata.keys())
+        return [from_networkx(ds_networkx)]
+        # return items
     elif name == 'aifb':
         return Entities("Datasets/aifb","AIFB")
     elif name == 'bgs':
         return Entities("Datasets/bgs","BGS")
     elif name == 'am':
         return Entities("Datasets/am", "AM")
+    else:
+        raise ValueError(name)
