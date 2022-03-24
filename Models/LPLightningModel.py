9260df8ec9d450c8c03c9c066cb05d2af3c403bf
Dylan Hillier <dylanslavinhillier@gmail.com>
2022-03-24 10:32:51 +0000

    updates lightning link prediction to actually work

--- Models/LPLightningModel.py
+++ Models/LPLightningModel.py
@@ -1,8 +1,8 @@
 from torch.nn.parameter import Parameter
-from torch import FloatTensor, LongTensor, as_tensor, norm, ones, long
+from torch import FloatTensor, LongTensor, as_tensor, norm, ones, long, full, sigmoid
 from torch.nn.functional import one_hot
 from pytorch_lightning import LightningModule
-from torch.nn import ModuleList, CrossEntropyLoss
+from torch.nn import ModuleList, BCELoss
 from torch import relu, softmax
 from torch.optim import Adam
 from Models.rgcn import RGCNLayer
@@ -17,7 +17,7 @@ class LinkPredictionRGCN(LightningModule):
         self.embedder = Parameter(FloatTensor(num_entities,in_dim))
         kaiming_normal_(self.embedder)
         self.layers = ModuleList([RGCNLayer(in_dim,hidden_dim,num_relations,**kwargs)]+[RGCNLayer(hidden_dim,hidden_dim, num_relations,**kwargs) for _ in range(num_layers-2)]+[RGCNLayer(hidden_dim,out_dim, num_relations)])
-        self.loss = CrossEntropyLoss(reduction='sum')
+        self.loss = BCELoss(reduction='sum')
         self.distmult = distMult(out_dim,num_relations)
         self.optimizer = optimizer
         self.omega = omega
@@ -33,8 +33,14 @@ class LinkPredictionRGCN(LightningModule):
             x = l(x, edge_index, edge_attributes)
         return x
 
+    def batch_edges(self, edges, edge_type, label, batch_size=10):
+        batched_edges = []
+        for i in range(0,edges.size(1),batch_size):
+            batch = (edges[:,i:i+batch_size],edge_type[i:i+batch_size],full((min(batch_size,edges.size(1)-i),),label))
+            batched_edges.append(batch)
+        return batched_edges
+
     def training_step(self, batch, batch_idx):
-        print(batch)
         edge_index = batch.train_edge_index
         edge_attributes = batch.train_edge_type
         edge_attributes = one_hot(edge_attributes,num_classes=self.num_relations)
@@ -43,16 +49,17 @@ class LinkPredictionRGCN(LightningModule):
             x = l(x, edge_index, edge_attributes)
         #### CODE UP TO HERE IS KINDA NASTY -- SHOULD WORK ON MAKING DATALOADERS MORE STANDARDISED...
         loss = 0
-        print("done with embedding")
         for _ in range(self.omega):
             edges = negative_sampling(edge_index,self.num_entities)
-            print("done sampling")
-            score = self.distmult(x[edges[0]],batch.edge_type,x[edges[1]]) ### Correct this
-            loss += self.loss(score,0)
-        print("done with neg samples")
-        edges = edge_attributes
-        score = self.distmult(x[edges[0]],edge_attributes[edges[0]],x[edges[1]])
-        loss += self.loss(score,1)/(edge_attributes.size(1)*(1+self.omega))
+            batches = self.batch_edges(edges,batch.train_edge_type,0.0)
+            for (edge,edge_attribute,label) in batches:
+                score = sigmoid(self.distmult(x[edge[0]],edge_attribute,x[edge[1]]))
+                loss += self.loss(score,label)/(edge_attribute.size(0)*(1+self.omega))
+        edges = edge_index
+        batches = self.batch_edges(edges,batch.train_edge_type,1.0)
+        for (edge, edge_attribute, label) in batches:
+            score = sigmoid(self.distmult(x[edge[0]],edge_attribute,x[edge[1]]))
+            loss += self.loss(score,label)/(edge_attribute.size(0)*(1+self.omega))
         for name, param in self.distmult.named_parameters(): ## L2 Loss
             loss += norm(param) * self.l2lambda
         self.log("train_loss",loss.item())
