6885f792ae5187bf416ed7089174383c7d608de1
Dylan Hillier <dylanslavinhillier@gmail.com>
2022-03-29 00:22:18 +0100

    updates entity classification model

--- Models/ECLightningModel.py
+++ Models/ECLightningModel.py
@@ -2,19 +2,24 @@ from torch import LongTensor, as_tensor, norm, ones, long
 from torch.nn.functional import one_hot
 from pytorch_lightning import LightningModule
 from torch.nn import ModuleList, CrossEntropyLoss
-from torch import relu, softmax
+from torch import relu, softmax, max
 from torch.optim import Adam
+import torchmetrics
 from Models.rgcn import RGCNLayer
+from torch_geometric.nn import RGCNConv
 class EntityClassificationRGCN(LightningModule):
     def __init__(self, num_layers, in_dim, hidden_dim, out_dim, num_relations, l2lambda=0.01, optimizer= Adam, lr=0.01,**kwargs):
         super(EntityClassificationRGCN,self).__init__()
         self.num_relations = num_relations
-        self.layers = ModuleList([RGCNLayer(in_dim,hidden_dim,num_relations,**kwargs)]+[RGCNLayer(hidden_dim,hidden_dim, num_relations,**kwargs) for _ in range(num_layers-2)]+[RGCNLayer(hidden_dim,out_dim, num_relations)])
+        # self.layers = ModuleList([RGCNConv(in_dim,hidden_dim,num_relations,bias=False,root_weight=True,aggr='add')]+[RGCNConv(hidden_dim,hidden_dim,num_relations,bias=False,root_weight=True,aggr='add') for _ in range(num_layers-2)]+[RGCNConv(hidden_dim,out_dim,num_relations,bias=False,root_weight=True,aggr='add')])
+        self.layers = ModuleList([RGCNLayer(in_dim,hidden_dim,num_relations,**kwargs)]+[RGCNLayer(hidden_dim,hidden_dim, num_relations,**kwargs) for _ in range(num_layers-2)]+[RGCNLayer(hidden_dim, out_dim, num_relations, activation=lambda x:x,**kwargs)])
         self.loss = CrossEntropyLoss(reduction='sum')
         self.optimizer = optimizer
         self.lr = lr
         self.l2lambda = l2lambda
         self.save_hyperparameters()
+        self.train_accuracy = torchmetrics.Accuracy()
+        self.test_accuracy = torchmetrics.Accuracy()
 
     def forward(self, x, edge_index, edge_attributes):
         for l in self.layers:
@@ -22,7 +27,6 @@ class EntityClassificationRGCN(LightningModule):
         return softmax(x,-1)
 
     def training_step(self, batch, batch_idx):
-        print(batch)
         x, edge_index, edge_attributes, y = batch.x, batch.edge_index, batch.edge_type, batch.train_y
         edge_attributes = one_hot(edge_attributes,num_classes=self.num_relations)
         if x is None:##If there are no initial features, just use a one to encode it
@@ -35,7 +39,30 @@ class EntityClassificationRGCN(LightningModule):
         for name, param in self.layers[0].named_parameters(): ## L2 Loss
             loss += norm(param) * self.l2lambda
         self.log("train_loss",loss.item())
+        ypred = max(x,-1).indices
+        self.train_accuracy(ypred,y)
+        self.log("train_acc",self.train_accuracy)
         return loss
+    def train_step_end(self, outs):
+        self.log("train_epoch_acc",self.train_accuracy)
+        
+
+    def test_step(self, batch, batch_idx):
+        x, edge_index, edge_attributes, y = batch.x, batch.edge_index, batch.edge_type, batch.test_y
+        edge_attributes = one_hot(edge_attributes,num_classes=self.num_relations)
+        if x is None:##If there are no initial features, just use a one to encode it
+            x = one_hot(as_tensor([i for i in range(batch.num_nodes)],dtype=long)).float()
+        for l in self.layers:
+            x = l(x, edge_index, edge_attributes)
+        #### CODE UP TO HERE IS KINDA NASTY -- SHOULD WORK ON MAKING DATALOADERS MORE STANDARDISED...
+        x = softmax(x[batch.test_idx],-1)
+        ypred = max(x,-1,).indices
+        print(x,ypred,y)
+        self.test_accuracy(ypred,y)
+        self.log("test_acc",self.test_accuracy)
+
+    def test_step_end(self, outs):
+        self.log("test_epoch_acc",self.test_accuracy)
 
     def configure_optimizers(self):
         return self.optimizer(self.parameters(),lr=self.lr)
