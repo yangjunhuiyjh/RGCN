f29332e9d4e66a2c6867aa6da7cbe2bfccef950f
Dylan Hillier <dylanslavinhillier@gmail.com>
2022-03-29 08:23:24 +0100

    updates experiment to have logging

--- experiment.py
+++ experiment.py
@@ -10,14 +10,16 @@
 from train import train_ec
 from DataLoaders.dataloader import get_dataset
 from torch_geometric.loader import DataLoader
-
+from pytorch_lightning.loggers import NeptuneLogger
 if __name__=='__main__':
     ds = get_dataset("aifb")
     dl = DataLoader(ds)
     num_nodes = ds[0].num_nodes
     results = []
-    for i in range(1):
-        model, trainer = train_ec(None,dl,50,num_nodes,90,num_bases=None,l2param=0,norm_type='relation-degree')
+    logger= NeptuneLogger(project='dylanslavinhillier/ATML',api_key="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJjZTZiNDUxYi03ZDNiLTQ3N2EtYjQwMC0wZjA0NTJiNTgwZDQifQ==")
+
+    for i in range(10):
+        model, trainer = train_ec(logger,dl,50,num_nodes,90,num_bases=None,l2param=0,norm_type='relation-degree')
         results.append(trainer.test(model,dl))
     for result in results:
         print(result)
