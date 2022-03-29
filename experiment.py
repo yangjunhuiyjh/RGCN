6ef9e03a241fcce56591d71296c1cc483be0fb55
Dylan Hillier <dylanslavinhillier@gmail.com>
2022-03-29 09:15:19 +0100

    setsup mutag experiment

--- experiment.py
+++ experiment.py
@@ -12,14 +12,14 @@ from DataLoaders.dataloader import get_dataset
 from torch_geometric.loader import DataLoader
 from pytorch_lightning.loggers import NeptuneLogger
 if __name__=='__main__':
-    ds = get_dataset("aifb")
+    ds = get_dataset("mutag")
     dl = DataLoader(ds)
     num_nodes = ds[0].num_nodes
     results = []
     logger= NeptuneLogger(project='dylanslavinhillier/ATML',api_key="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJjZTZiNDUxYi03ZDNiLTQ3N2EtYjQwMC0wZjA0NTJiNTgwZDQifQ==")
 
     for i in range(10):
-        model, trainer = train_ec(logger,dl,50,num_nodes,90,num_bases=None,l2param=0,norm_type='relation-degree')
+        model, trainer = train_ec(logger,dl,50,num_nodes,46,num_bases=30,l2param=5e-4,norm_type='relation-degree')
         results.append(trainer.test(model,dl))
     for result in results:
         print(result)
