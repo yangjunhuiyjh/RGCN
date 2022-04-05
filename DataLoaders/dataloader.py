86311b9866da386eadfd3dcfcc805f977d11b429
lequocminh1999 <lequocminh1999@gmail.com>
2022-04-05 23:59:31 +0100

    Adding path to dataloader

--- DataLoaders/dataloader.py
+++ DataLoaders/dataloader.py
@@ -3,9 +3,12 @@ from dgl import to_networkx
 from dgl.data import FB15kDataset
 import torch
 from torch_geometric.utils.convert import from_networkx
+import os
 
 # Valid names are:
 # fb15k237, wn18, fb15k, mutag, aifb, bgs, am
 
+
 def get_dataset(name: str):
-    return [torch.load(name+".pt")]
+    dirname = os.path.dirname(__file__)
+    return [torch.load(os.path.join(dirname,name+".pt"))]
